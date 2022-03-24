19001395d81c48cacb289a4db679968831ab3120
Taehoon Ha <ha@cshl.edu>
2022-03-24 18:40:14 -0400

    Update 1988_Find_Cutoff_Score_for_Each_School.sql

--- solution/1988_Find_Cutoff_Score_for_Each_School.sql
+++ solution/1988_Find_Cutoff_Score_for_Each_School.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 select school_id, ifnull(min(score), -1) as score
-    from Schools left join Exam
+from Schools 
+left join Exam
     on capacity >= student_count
-    group by school_id;
+group by school_id;
