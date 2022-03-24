bde5ad7e7bb6f819cace0358a839b11f74ad47c3
Taehoon Ha <ha@cshl.edu>
2022-03-24 18:36:20 -0400

    Update 1907_Count_Salary_Categories.sql

--- solution/1907_Count_Salary_Categories.sql
+++ solution/1907_Count_Salary_Categories.sql
@@ -1,6 +1,16 @@
 # Write your MySQL query statement below
-select "Low Salary" as category, count(*) as accounts_count from Accounts where income < 20000
+select "Low Salary" as category, count(*) as accounts_count 
+from Accounts 
+where income < 20000
+
 union 
-select "Average Salary" as category, count(*) as accounts_count from Accounts where 20000 <= income and income <= 50000
+
+select "Average Salary" as category, count(*) as accounts_count 
+from Accounts 
+where 20000 <= income and income <= 50000
+
 union
-select "High Salary" as category, count(*) as accounts_count from Accounts where 50000 < income;
+
+select "High Salary" as category, count(*) as accounts_count 
+from Accounts 
+where 50000 < income;
