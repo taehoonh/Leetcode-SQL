a68b9cf025e96dd472e872b3a89ce64d08599742
Taehoon Ha <ha@cshl.edu>
2022-03-24 18:48:12 -0400

    Update 2084_Drop_Type_1_Orders_for_Customers_With_Type_0_Orders.sql

--- solution/2084_Drop_Type_1_Orders_for_Customers_With_Type_0_Orders.sql
+++ solution/2084_Drop_Type_1_Orders_for_Customers_With_Type_0_Orders.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 SELECT * FROM Orders
-WHERE (customer_id, order_type) 
-IN (SELECT customer_id, MIN(order_type) 
-    FROM Orders 
-    GROUP BY customer_id);
+WHERE (customer_id, order_type) IN (SELECT customer_id, MIN(order_type) 
+                                    FROM Orders 
+                                    GROUP BY customer_id);
