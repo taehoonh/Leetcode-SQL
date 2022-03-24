b656e0d7428597c662f03a68291b4d5746e5f088
Taehoon Ha <ha@cshl.edu>
2022-03-24 18:50:02 -0400

    Update 2118_Build_the_Equation.sql

--- solution/2118_Build_the_Equation.sql
+++ solution/2118_Build_the_Equation.sql
@@ -1,15 +1,10 @@
 # Write your MySQL query statement below
-select
-concat(group_concat(term order by power desc separator ''), '=0') as equation
-from (
-select
-CONCAT(case when factor > 0 then '+' else '' end, 
-       factor, 
-       case when power = 0 then '' else 'X' end, 
-       case when power = 0 or power = 1 then '' else '^' end, 
-       case when power = 0 or power = 1 then '' else power end
-) term,
-power
-from
-terms
-order by power desc) t;
+select concat(group_concat(term order by power desc separator ''), '=0') as equation
+from (select CONCAT(case when factor > 0 then '+' else '' end, 
+                    factor, 
+                    case when power = 0 then '' else 'X' end, 
+                    case when power = 0 or power = 1 then '' else '^' end, 
+                    case when power = 0 or power = 1 then '' else power end) term,
+      power
+       from terms
+       order by power desc) t;
