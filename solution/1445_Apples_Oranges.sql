1af0638e86479379760ed7f8580b60725b597f60
Taehoon Ha <ha@cshl.edu>
2022-03-31 16:49:46 -0400

    Update 1445_Apples_Oranges.sql

--- solution/1445_Apples_Oranges.sql
+++ solution/1445_Apples_Oranges.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-select date(sale_date) as sale_date,
-       sum(case when fruit = 'apples' then sold_num
-                when fruit = 'oranges' then -sold_num end) as diff
-from Sales
-group by 1
-order by 1
-;
+SELECT
+	date(sale_date) AS sale_date,
+	sum(CASE WHEN fruit = 'apples' THEN sold_num WHEN fruit = 'oranges' THEN -sold_num END) AS diff
+FROM
+	Sales
+GROUP BY
+	1
+ORDER BY
+	1 ;
