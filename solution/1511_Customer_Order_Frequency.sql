9c67a387df894794dd847a8e07d2b97039621679
Taehoon Ha <ha@cshl.edu>
2022-03-30 22:22:56 -0400

    Update 1511_Customer_Order_Frequency.sql

--- solution/1511_Customer_Order_Frequency.sql
+++ solution/1511_Customer_Order_Frequency.sql
@@ -1,17 +1,16 @@
 # Write your MySQL query statement below
-select o.customer_id, name
-from
-Orders o
-join
-Product p
-on o.product_id = p.product_id
-join
-Customers c
-on o.customer_id = c.customer_id
-group by 1, 2
-having sum(case when date_format(order_date, '%Y-%m')='2020-06'
-then price*quantity end) >= 100
-and 
-sum(case when date_format(order_date, '%Y-%m')='2020-07'
-then price*quantity end) >= 100
-;
+SELECT
+	o.customer_id,
+	name
+FROM
+	Orders o
+JOIN Product p ON
+	o.product_id = p.product_id
+JOIN Customers c ON
+	o.customer_id = c.customer_id
+GROUP BY
+	1,
+	2
+HAVING
+	sum(CASE WHEN date_format(order_date, '%Y-%m')= '2020-06' THEN price*quantity END) >= 100
+	AND sum(CASE WHEN date_format(order_date, '%Y-%m')= '2020-07' THEN price*quantity END) >= 100;
